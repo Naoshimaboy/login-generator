6268dba81d2f9de0ad82e51ee263703960d058e0
rodriguez nils <rdn2513a@203-12l.ens.fsi>
2019-11-20 09:45:41 +0100

    avant de partir

---
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -0,0 +1,19 @@
+package geco;
+import org.junit.Before;
+import org.junit.Test;
+
+public class LoginGeneratorTest {
+
+    @Before
+    public void initialiser()  {
+        LoginService test = new LoginService(new String[] {"JROL","BPER", "CGUR", "JDU", "JRAL", "JRAL1"});
+    }
+
+
+    @Test
+    public void CT1() {
+
+    }
+
+
+}
