011b94c2cd4b16fc1a1c31e3071d672520e376dc
rodriguez nils <rdn2513a@209-12l.ens.fsi>
2019-11-22 09:24:11 +0100

    password generation

---
+++ src/main/java/geco/PasswordGeneration.java
@@ -0,0 +1,33 @@
+package geco;
+
+import java.util.Random;
+
+public class PasswordGeneration {
+    int seed;
+    /**
+     * Genere un mdp aléatoire.
+     * @return le mdp
+     */
+
+    public PasswordGeneration(){
+        this.seed = 0;
+    }
+
+
+    public String generateRandomPassword() {
+
+        int leftLimit = 97; // letter 'a'
+        int rightLimit = 122; // letter 'z'
+        int targetStringLength = 8;
+        Random random = new Random();
+        StringBuilder buffer = new StringBuilder(targetStringLength);
+        for (int i = 0; i < targetStringLength; i++) {
+            int randomLimitedInt = leftLimit + (int)
+                    (random.nextFloat() * (rightLimit - leftLimit + 1));
+            buffer.append((char) randomLimitedInt);
+        }
+        String generatedString = buffer.toString();
+        return generatedString;
+
+    }
+}
